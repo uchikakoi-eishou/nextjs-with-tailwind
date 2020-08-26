d75962c984ffb8d96439ca4cd4084bb2b35f0a62
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2020-08-26 22:26:36 +0900

    最初のコミット

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import React from 'react'
+import App from 'next/app'
+import '../css/tailwind.css'
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
 }
 
 export default MyApp
